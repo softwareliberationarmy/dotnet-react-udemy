182b9c8fe81db8e19082d24308f6b1889eafcd9b
Kerry Patrick <kpatrick@mmmicrosystems.com>
2023-09-08 10:27:19 -0500

    reverting Cancellation token logic

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -9,9 +9,9 @@ namespace API.Controllers
     public class ActivitiesController : BaseApiController
     {
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> GetActivities(CancellationToken ct)
+        public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await Mediator.Send(new List.Query(), ct);
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -16,29 +16,14 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Query, List<Activity>>
         {
             private readonly DataContext _context;
-            private readonly ILogger<List> _logger;
-            public Handler(DataContext context, ILogger<List> logger)
+            public Handler(DataContext context)
             {
-                this._logger = logger;
                 this._context = context;
             }
 
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
-                try
-                {
-                    for (int i = 0; i < 10; i++)
-                    {
-                        cancellationToken.ThrowIfCancellationRequested();
-                        await Task.Delay(5000, cancellationToken);
-                        _logger.LogInformation($"Task {i} has completed");
-                    }
-                }
-                catch (System.Exception)
-                {
-                    _logger.LogInformation("Task was canceled");
-                }
-                return await _context.Activities.ToListAsync(cancellationToken);
+                return await _context.Activities.ToListAsync();
             }
         }
     }
