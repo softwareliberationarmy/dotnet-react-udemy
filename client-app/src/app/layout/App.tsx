5035ca6489a9a348535cb3136cf3b68771b77573
Kerry Patrick <kpatrick@mmmicrosystems.com>
2023-09-14 15:31:36 -0500

    adding observable porperty from our ActivityStore to the App.tsx as a demo, watching it refresh when we execute an action on the store

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from 'react';
-import { Container } from 'semantic-ui-react';
+import { Button, Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import { v4 as uuid } from 'uuid';
 import agent from '../api/agent';
 import LoadingComponent from './LoadingComponents';
+import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite';
 
 function App() {
+  const { activityStore } = useStore();
+
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<
     Activity | undefined
@@ -74,7 +78,7 @@ function App() {
     agent.Activities.delete(id).then(() => {
       setActivities([...activities.filter((x) => x.id !== id)]);
       setSubmitting(false);
-    })
+    });
   }
 
   if (loading) {
@@ -85,6 +89,12 @@ function App() {
     <>
       <NavBar openForm={handleFormOpen} />
       <Container style={{ marginTop: '7em' }}>
+        <h2>{activityStore.title}</h2>
+        <Button
+          content="Add Exclamation"
+          positive
+          onClick={activityStore.setTitle}
+        />
         <ActivityDashboard
           activities={activities}
           selectedActivity={selectedActivity}
@@ -102,4 +112,4 @@ function App() {
   );
 }
 
-export default App;
+export default observer(App);
--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,11 +1,15 @@
-import { makeObservable, observable } from "mobx";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { makeAutoObservable } from 'mobx';
 
 export default class ActivityStore {
-    title = 'Hello from Mobx!';
+  title = 'Hello from Mobx!';
 
-    constructor() {
-        makeObservable(this, {
-            title: observable
-        })
-    }
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  //make sure is an arrow function so we don't need to bind the action in the ctor
+  setTitle = () => {
+    this.title = this.title + '!';
+  };
 }
