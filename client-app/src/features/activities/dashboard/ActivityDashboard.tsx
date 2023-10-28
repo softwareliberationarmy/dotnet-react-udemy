b60b23463cb54a5de67e8ccc34298559e57072b2
Kerry Patrick <kpatrick@mmmicrosystems.com>
2023-10-28 09:14:33 -0500

    End of section 9

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,9 +1,10 @@
-import { Grid } from 'semantic-ui-react';
-import ActivityList from './ActivityList';
-import { useStore } from '../../../app/stores/store';
-import { observer } from 'mobx-react-lite';
-import { useEffect } from 'react';
-import LoadingComponent from '../../../app/layout/LoadingComponents';
+import { Grid } from "semantic-ui-react";
+import ActivityList from "./ActivityList";
+import { useStore } from "../../../app/stores/store";
+import { observer } from "mobx-react-lite";
+import { useEffect } from "react";
+import LoadingComponent from "../../../app/layout/LoadingComponents";
+import ActivityFilters from "./ActivityFilters";
 
 export default observer(function ActivityDashboard() {
   const { activityStore } = useStore();
@@ -25,7 +26,7 @@ export default observer(function ActivityDashboard() {
         <ActivityList />
       </Grid.Column>
       <Grid.Column width="6">
-        <h2>Activity Filters</h2>
+        <ActivityFilters />
       </Grid.Column>
     </Grid>
   );
---
+++ client-app/src/features/activities/dashboard/ActivityFilters.tsx
@@ -0,0 +1,18 @@
+import { Fragment } from "react";
+import Calendar from "react-calendar";
+import { Menu, Header } from "semantic-ui-react";
+
+export default function ActivityFilters() {
+  return (
+    <Fragment>
+      <Menu vertical size="large" style={{ width: "100%", marginTop: 30 }}>
+        <Header icon="filter" attached color="teal" content="Filters" />
+        <Menu.Item content="All Activities" />
+        <Menu.Item content="I'm going" />
+        <Menu.Item content="I'm hosting" />
+      </Menu>
+      <Header />
+      <Calendar />
+    </Fragment>
+  );
+}
